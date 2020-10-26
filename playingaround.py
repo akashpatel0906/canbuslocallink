f410d91bf02f00c20bfcad24fb77bad4d1fe336a
akashpatel0906 <akashpatel0906@yahoo.com>
2020-10-25 23:13:16 -0500

    Update playingaround.py

--- playingaround.py
+++ playingaround.py
@@ -8,12 +8,12 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
-external_stylesheets = ['https://filebin.net/n23827xxj36naw6o']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server=app.server
 
-df = pd.read_csv('/Users/akashpatel/Documents/FormulaTest/testnew.csv')
+df = pd.read_csv('https://filebin.net/n23827xxj36naw6o/testnew.csv?t=c274nyb4')
 
 fig = px.line(df, x = 'Timestamp', y = 'Value(DEC)', color='Name', title='CANBus Data Values')
 
@@ -33,5 +33,6 @@ app.layout = html.Div(children=[
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
 
 
